1af20f1b6bd6817d5db36ddaecb7d7c11038a025
reddspitt <reddysupardi@gmail.com>
2023-02-25 08:50:02 +0700

    add work

--- main.js
+++ main.js
@@ -11,6 +11,5 @@ function toggleDetail(e) {
     
     const item = $(target).parents(".about-exp-item")
     const detail = $(item).children(".about-exp-item-detail")
-
     $(detail).slideToggle()
 }
